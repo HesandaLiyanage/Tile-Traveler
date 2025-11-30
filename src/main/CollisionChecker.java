da189cb3724128b9640244d587f11e31f81eab73
Hesanda Liyanage <nhesanda@gmail.com>
2025-11-30 12:17:35 +0530

    11

--- src/main/CollisionChecker.java
+++ src/main/CollisionChecker.java
@@ -15,5 +15,11 @@ public class CollisionChecker {
         int entityLeftWorldX = entity.worldX + entity.solidarea.x;
         int entityRightWorldX = entity.worldX + entity.solidarea.x +  entity.solidarea.width;
         int entityTopWorldY = entity.worldY + entity.solidarea.y;
+        int entityBottomWorldY = entity.worldY + entity.solidarea.y + entity.solidarea.height;
+
+        int entityLeftCol = entityLeftWorldX/gp.tileSize;
+        int entityRightCol = entityRightWorldX/gp.tileSize;
+        int entityTopRow = entityTopWorldY/gp.tileSize;
+        int entityBottomRow = entityBottomWorldY/gp.tileSize;
     }
 }
