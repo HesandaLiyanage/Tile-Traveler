a90707d0b24333b3b8b20c4c504d7fc6ff369d39
Hesanda Liyanage <nhesanda@gmail.com>
2025-11-30 12:02:35 +0530

    5

--- src/main/CollisionChecker.java
+++ src/main/CollisionChecker.java
@@ -1,8 +1,17 @@
 package main;
 
+import entity.Entity;
+
 public class CollisionChecker {
+
+    Gamepanel gp;
 
     public CollisionChecker(Gamepanel gp ) {
         this.gp = gp;
+    }
+
+    public void checkTile(Entity entity) {
+
+
     }
 }
